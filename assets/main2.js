82910d072cfdbfc07c663c8dee92e9595d4717f8
Lamuel27 <samrelliner@gmail.com>
2021-02-10 11:19:40 -0600

    switcharu

--- assets/main2.js
+++ assets/main2.js
@@ -6,11 +6,12 @@ var ddaPercentage = document.getElementById('ddaPercentage');
 var customerValue = document.getElementById('customerValue');
 var customerRetention = document.getElementById('customerRetention');
 var costPerSwitch = document.getElementById('costPerSwitch');
+var newDepositAdoption = document.getElementById('newDepositAdoption');
 
 
 function calculateRoi() {
     // Get result fields
-    var var1= document.getElementById('newDepositAdoption');
+    // var var1= document.getElementById('newDepositAdoption');
     var var2= document.getElementById('incRevenueResult');
     var var3= document.getElementById('costResult');
     var var4= document.getElementById('profitIncreaseResult');
@@ -21,15 +22,15 @@ function calculateRoi() {
     var var9= document.getElementById('accountLength');
 
     // Calculate
-    var newDepositAdoption= '';
-        if (ddaPercentage.value/100 > 0.6) {
-            newDepositAdoption = (ddaPercentage.value/100 * 1.3);
-        }
-        else {
-            newDepositAdoption = (ddaPercentage.value/100 + 0.2);
-        }
-    var incRevenueResult= (((newDepositAdoption - (ddaPercentage.value / 100)) * newDDA.value) * customerValue.value);
-    var costResult= ((newDepositAdoption) * costPerSwitch.value * newDDA.value);
+    // var newDepositAdoption= '';
+    //     if (ddaPercentage.value/100 > 0.6) {
+    //         newDepositAdoption = (ddaPercentage.value/100 * 1.3);
+    //     }
+    //     else {
+    //         newDepositAdoption = (ddaPercentage.value/100 + 0.2);
+    //     }
+    var incRevenueResult= ((((newDepositAdoption.value / 100) - (ddaPercentage.value / 100)) * newDDA.value) * customerValue.value);
+    var costResult= ((newDepositAdoption.value / 100) * costPerSwitch.value * newDDA.value);
     var profitIncreaseResult= (incRevenueResult - costResult);
     var costPaybackResult= ((costResult / incRevenueResult) * 12);
     var irrResult= incRevenueResult / costResult * 100;
@@ -38,7 +39,6 @@ function calculateRoi() {
     var accountLength= 'Value added per account over ' + customerRetention.value + ' year relationship';
     // Apply results
 
-    var1.innerHTML= newDepositAdoption.toFixed(2) * 100;
     var2.innerHTML= incRevenueResult.toLocaleString('en-US', {style:'currency', currency:'USD'});
     var3.innerHTML= costResult.toLocaleString('en-US', {style:'currency', currency:'USD'});
     var4.innerHTML= profitIncreaseResult.toLocaleString('en-US', {style:'currency', currency:'USD'});
@@ -48,7 +48,6 @@ function calculateRoi() {
     var8.innerHTML= irrlength;
     var9.innerHTML= accountLength;
 
-    $(var1).append('%');
     $(var5).append(' months');
     $(var6).append('%');
  
@@ -61,6 +60,19 @@ $('.roiAnalizerContainer').on('click','#ctaCalculate',function(){
     
  });
 
-
+ var doc = new jsPDF();
+ var specialElementHandlers = {
+     '#editor': function (element, renderer) {
+         return true;
+     }
+ };
+ 
+ $('#cmd').click(function () {
+     doc.fromHTML($('#content').html(), 15, 15, {
+         'width': 170,
+             'elementHandlers': specialElementHandlers
+     });
+     doc.save('ClickSWITCHROI.pdf');
+ });
 
 });
